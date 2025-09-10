258278f03ce7c1e60eecd319faa515c946065093
cypherdavy <166112437+cypherdavy@users.noreply.github.com>
2025-09-10 23:18:33 +0530

    Update main.py

--- yumi/main.py
+++ yumi/main.py
@@ -1,1 +1,13 @@
+import argparse
+from yumi.core.engine import Engine
 
+def start():
+    parser = argparse.ArgumentParser(description="Yumi - An intelligent, asynchronous JS Recon Engine")
+    parser.add_argument("domain", help="Target domain for reconnaissance")
+    args = parser.parse_args()
+
+    engine = Engine(args.domain)
+    engine.run()
+
+if __name__ == "__main__":
+    start()
