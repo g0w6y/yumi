dc1c6e72a74d1abadd35b7730dba7a4c144c5a64
cypherdavy <166112437+cypherdavy@users.noreply.github.com>
2025-09-10 23:20:31 +0530

    Update recon.py

--- yumi/modules/recon.py
+++ yumi/modules/recon.py
@@ -1,1 +1,19 @@
+import httpx
+from rich.console import Console
 
+console = Console()
+
+class Recon:
+    def __init__(self, domain):
+        self.domain = domain
+        self.subdomains = set()
+
+    def enumerate_subdomains(self):
+    
+        self.subdomains.add(self.domain)
+        self.subdomains.add(f"www.{self.domain}")
+        self.subdomains.add(f"dev.{self.domain}")
+        self.subdomains.add(f"api.{self.domain}")
+
+        console.log(f"Found {len(self.subdomains)} subdomains.")
+        return list(self.subdomains)
