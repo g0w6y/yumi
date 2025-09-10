536e43be20bb75c25cccc21253dc36249a0c4592
cypherdavy <166112437+cypherdavy@users.noreply.github.com>
2025-09-10 23:22:19 +0530

    Update logger.py

--- yumi/utils/logger.py
+++ yumi/utils/logger.py
@@ -1,1 +1,14 @@
+from rich.console import Console
 
+class Logger:
+    def __init__(self):
+        self.console = Console()
+
+    def info(self, message):
+        self.console.log(f"[bold blue]INFO[/bold blue] - {message}")
+
+    def warning(self, message):
+        self.console.log(f"[bold yellow]WARNING[/bold yellow] - {message}")
+
+    def error(self, message):
+        self.console.log(f"[bold red]ERROR[/bold red] - {message}")
