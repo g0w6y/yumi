2431ea5ef190a6494b0d4389200374d1ddb2c18f
cypherdavy <166112437+cypherdavy@users.noreply.github.com>
2025-09-10 22:52:11 +0530

    Delete plugins directory

--- plugins/__init__.py
+++
@@ -1,9 +0,0 @@
-from .custom_patterns import scan_content_for_custom_patterns
-
-def run_plugins_scan(content):
-    """
-    Run all plugin scans on the provided content.
-    """
-    findings = []
-    findings.extend(scan_content_for_custom_patterns(content))
-    return findings
